ed38a8639ffdf2b6a1356cccff0ec492ce94535d
Sauloxd45 <sauloxd45@hotmail.com>
2025-02-14 15:00:29 -0600

    seccion de post ya se migraron las base de datos minutot 53.37

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='blog/images', null=True, blank=True)
+    date=models.DateTimeField(datetime.datetime.today)
+    
+    def __str__(self):
+        return self.title
