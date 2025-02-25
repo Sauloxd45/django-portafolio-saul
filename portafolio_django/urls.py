87279e6624ef8c899eff5947b9324c327db88559
Sauloxd45 <sauloxd45@hotmail.com>
2025-02-25 10:43:05 -0600

    Ahora si ya es para emepsar a juntar los formatos del viejo portafolio con DJango

--- portafolio_django/urls.py
+++ portafolio_django/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 from portafolio  import views
@@ -26,7 +26,7 @@ urlpatterns = [
     path('blog/',include('blog.urls')),
 ]
 
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     
-""" ya ya dejar de mamadas y pon el path de la app """
+    """ ya ya dejar de mamadas y pon el path de la app """
